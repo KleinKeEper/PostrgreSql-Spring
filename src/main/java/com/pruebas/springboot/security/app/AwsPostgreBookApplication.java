30667113f80c67457864b70e3a1d70d6158775c1
KleinKeEper <kranmerramos@upeu.edu.pe>
2020-11-11 10:35:42 -0500

    joder

---
+++ src/main/java/com/pruebas/springboot/security/app/AwsPostgreBookApplication.java
@@ -0,0 +1,13 @@
+package com.pruebas.springboot.security.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPostgreBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPostgreBookApplication.class, args);
+	}
+
+}
