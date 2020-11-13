e2d559601e381cc01e70096448238a7968dc3c82
KleinKeEper <kranmerramos@upeu.edu.pe>
2020-11-12 21:30:02 -0500

    ul

--- src/main/java/com/pruebas/springboot/security/app/service/BookService.java
+++ src/main/java/com/pruebas/springboot/security/app/service/BookService.java
@@ -7,7 +7,5 @@ import com.pruebas.springboot.security.app.Model.Book;
 
 public interface BookService {
 
-	Map<String, Object> getAll();
-	List<Book> getList();
 	List<Map<String, Object>> get();
 }
