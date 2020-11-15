592fb086989e9179f1c9679e0810db723e7668f2
klein <klein@pop-os.localdomain>
2020-11-15 12:29:48 -0500

    sd

--- src/main/java/com/pruebas/springboot/security/app/controller/HomeController.java
+++ src/main/java/com/pruebas/springboot/security/app/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,16 @@ import com.pruebas.springboot.security.app.Model.Book;
 import com.pruebas.springboot.security.app.service.BookService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class HomeController {
 
 	@Autowired
 	private BookService bookService;
 	
-	@GetMapping("")
+	@GetMapping("home")
 	public String home() {
-		return "Hello ";
+		return "{status: ok}";
 	}
 	
 	
